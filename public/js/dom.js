e0dc0d52221f513de85282f5c5d1ba3f4546dbb5
momenmarwan <momenalgaga82@gmail.com>
2023-04-05 22:07:04 +0300

    append the event to data base from form

---
+++ public/js/dom.js
@@ -0,0 +1,9 @@
+const eventNameInput = document.getElementById('event-name');
+const locationInput = document.getElementById('location');
+const eventDateInput = document.getElementById('event-date');
+const eventDetails = document.getElementById('event-details');
+const btnAddEvent = document.getElementById('add-event');
+
+
+
+

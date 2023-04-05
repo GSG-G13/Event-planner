3baa3ee99826588821ea2951d06dca1e2a022a4c
momenmarwan <momenalgaga82@gmail.com>
2023-04-05 21:15:33 +0300

    make a html and css and serve them from backen

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = require("./routes/routes");
+const path = require('path')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,"..","public")))
 
 app.use(router);
 
